03fe8fec91d4630e01117644cc872c64323abea6
Snoopytoop <samuelluke1998@gmail.com>
2023-07-03 19:31:14 +0100

    init repo

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function about() {
+  return (
+    <div>
+      <Head>
+        <title>WebDev News</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+      <h1>About</h1>
+    </div>
+  );
+}
+
+export default about;
